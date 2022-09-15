10ec0ce43ad94f075734e19f5bc7ef65e30697fe
Gama´s Product <Gamas Product@DESKTOP-S14JKI4>
2022-09-14 17:09:19 -0700

    Agregar archivos de proyecto.

---
+++ practica2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 22;
+            string nombre = "alan";
+            string pais = "mexico";
+
+            Console.WriteLine("mi edad es: {0} luego mi nombre es: {1} y finalmente soy de: {2}", edad, nombre, pais);
+            Console.ReadLine();
+
+
+        }
+    }
+}
